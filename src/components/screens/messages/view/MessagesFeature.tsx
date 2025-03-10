d859c83ee2285ebf737771e3cfe17105abf857e7
LVNAnh <levunhatanh1997@gmail.com>
2025-03-10 16:32:29 +0700

    cap nhat responsive cho message

--- src/components/screens/messages/view/MessagesFeature.tsx
+++ src/components/screens/messages/view/MessagesFeature.tsx
@@ -278,7 +278,7 @@ const MessagesFeature = () => {
               messagesEndRef.current?.scrollIntoView({ behavior: 'instant' });
               setShowScrollToBottom(false);
             }}
-            className="absolute bottom-16 md:bottom-20 right-6 md:right-12 p-1 md:p-2 bg-white border border-gray-300 rounded-full shadow-md hover:bg-gray-200"
+            className="absolute bottom-16 md:bottom-20 md:mb-2 right-6 md:right-12 p-1 md:p-2 bg-white border border-gray-300 rounded-full shadow-md hover:bg-gray-200"
             title={localStrings.Messages.ScrollToBottom}
           >
             <CiCircleChevDown className="text-xl md:text-2xl text-gray-700" />
