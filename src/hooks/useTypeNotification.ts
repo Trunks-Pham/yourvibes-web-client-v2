4e1d291d2cc9a9aea4e7bceec693f7d71c7bd83c
Thanh-Phuog <phuongttt.125010122029@vtc.edu.vn>
2025-04-14 16:34:36 +0700

    socket

--- src/hooks/useTypeNotification.ts
+++ src/hooks/useTypeNotification.ts
@@ -5,7 +5,13 @@ const useTypeNotification = () => {
     const FRIEND_REQUEST ="friend_request";
     const ACCEPT_FRIEND_REQUEST ="accept_friend_request";
     const NEW_POST ="new_post";
+    const NEW_POST_PERSONAL ="new_post_personal";
+    const BLOCK_CREATE_POST ="block_create_post";
     const LIKE_COMMENT ="like_comment";
+    const DEACTIVATE_POST ="deactivate_post";
+    const ACTIVACE_POST ="activace_post";
+    const DEACTIVATE_COMMENT ="deactivate_comment";
+    const ACTIVACE_COMMENT ="activace_comment";
     return{
         LIKE_POST,
         NEW_SHARE,
@@ -13,7 +19,14 @@ const useTypeNotification = () => {
         FRIEND_REQUEST,
         ACCEPT_FRIEND_REQUEST,
         NEW_POST,
-        LIKE_COMMENT
+        LIKE_COMMENT,
+        NEW_POST_PERSONAL,
+        BLOCK_CREATE_POST,
+        DEACTIVATE_POST,
+        ACTIVACE_POST,
+        DEACTIVATE_COMMENT,
+        ACTIVACE_COMMENT,
+
     }
 }
 export default useTypeNotification;
