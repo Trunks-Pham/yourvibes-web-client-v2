2cdebeddb91bb7a001260120a37bbbe8a3b8d398
MTPE <mtpe2301@gmail.com>
2025-05-20 21:35:03 +0700

    Ads mana 20/05/25

--- src/components/screens/adsManagement/view/adsManagement.tsx
+++ src/components/screens/adsManagement/view/adsManagement.tsx
@@ -241,22 +241,6 @@ const AdDetailsModal = ({ ad, onClose, post, postId, adsForPost }: { ad: MappedA
             </div>
           </div>
         </div>
-        <div className="mt-2 text-center flex justify-center gap-4">
-          {adsForPost.length > 1 && (
-            <Button
-              className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
-              onClick={openHistoryModal}
-            >
-              {localStrings.Ads.ViewHistory} ({adsForPost.length})
-            </Button>
-          )}
-          <Button
-            className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600"
-            onClick={handleCloseModal}
-          >
-            {localStrings.Public.Close}
-          </Button>
-        </div>
       </div>
     </div>
   );
